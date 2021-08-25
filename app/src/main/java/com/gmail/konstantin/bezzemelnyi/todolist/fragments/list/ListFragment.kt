f1ade13ac2f3bfe5bd07e2d82d139293b83c9bed
constzz <konstantin.bezzemelnyi@gmail.com>
2021-08-25 10:52:00 +0300

    change list layout from Linear to StaggeredGrid with 2 of spanCount

--- app/src/main/java/com/gmail/konstantin/bezzemelnyi/todolist/fragments/list/ListFragment.kt
+++ app/src/main/java/com/gmail/konstantin/bezzemelnyi/todolist/fragments/list/ListFragment.kt
@@ -7,7 +7,7 @@ import android.widget.Toast
 import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.gmail.konstantin.bezzemelnyi.todolist.R
 import com.gmail.konstantin.bezzemelnyi.todolist.common.extensions.swipeToDeleteForItems
 import com.gmail.konstantin.bezzemelnyi.todolist.data.viewmodel.ToDoViewModel
@@ -39,7 +39,8 @@ class ListFragment : Fragment() {
 
         val recyclerView = binding.recyclerView
         recyclerView.adapter = adapter
-        recyclerView.layoutManager = LinearLayoutManager(requireActivity())
+        recyclerView.layoutManager =
+            StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
         recyclerView.itemAnimator = OvershootInLeftAnimator()
         recyclerView.swipeToDeleteForItems { viewHolderPosition ->
             val toDoEntityToDelete = adapter.getToDoEntity(viewHolderPosition)
